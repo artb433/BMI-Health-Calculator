236cfb44eec3b141cd57960c8869ad6f9f4a114b
Art b <94374097+artb433@users.noreply.github.com>
2022-01-16 16:19:23 +0000

    my

--- health_checker/lib/input_page.dart
+++ health_checker/lib/input_page.dart
@@ -12,12 +12,59 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: const Text('BMI CALCULATOR'),
       ),
-      body: Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
-          color: const Color(0xFF1D1E33),
-          height: 200,
-          width: 170,
-          margin: const EdgeInsets.all(15)),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: const Color(0xFF1D1E33),
+                        borderRadius: BorderRadius.circular(15)),
+                    height: 200,
+                    width: 170,
+                    margin: const EdgeInsets.all(15)),
+                Container(
+                    decoration: BoxDecoration(
+                        color: const Color(0xFF1D1E33),
+                        borderRadius: BorderRadius.circular(15)),
+                    height: 200,
+                    width: 170,
+                    margin: const EdgeInsets.all(15)),
+              ],
+            ),
+          ),
+          Container(
+              decoration: BoxDecoration(
+                  color: const Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(15)),
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.all(15)),
+          Expanded(
+            child: Row(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: const Color(0xFF1D1E33),
+                        borderRadius: BorderRadius.circular(15)),
+                    height: 200,
+                    width: 170,
+                    margin: const EdgeInsets.all(15)),
+                Expanded(
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: const Color(0xFF1D1E33),
+                          borderRadius: BorderRadius.circular(15)),
+                      height: 200,
+                      width: 170,
+                      margin: const EdgeInsets.all(15)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.add),
