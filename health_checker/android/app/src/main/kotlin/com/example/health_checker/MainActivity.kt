15e73863224bdbec469af3cfbbbdaba541ef5bd5
Art b <94374097+artb433@users.noreply.github.com>
2022-01-16 15:39:33 +0000

    my

---
+++ health_checker/android/app/src/main/kotlin/com/example/health_checker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.health_checker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ health_checker/lib/input_page.dart
@@ -0,0 +1,27 @@
+// import 'package:flutter/material.dart';
+
+// class InputPage extends StatefulWidget {
+//   @override
+//   _InputPageState createState() => _InputPageState();
+// }
+
+// class _InputPageState extends State<InputPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('BMI CALCULATOR'),
+//       ),
+//       body: Container(
+//           decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
+//           color: const Color(0xFF1D1E33),
+//           height: 200,
+//           width: 170,
+//           margin: const EdgeInsets.all(15)),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+}
