6f88e9c737fb6640a172184937efef4d456b6af0
Art b <94374097+artb433@users.noreply.github.com>
2022-01-17 22:43:32 +0000

    my

---
+++ health_checker/lib/constants.dart/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+const inactiveCardColor = Color(0xFF111328);
+const labelTextStyle = TextStyle(fontSize: 18, color: Colors.white);
