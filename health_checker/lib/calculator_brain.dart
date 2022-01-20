85bdd04cf0b1ad1f1437dc70ace85a1de0e36d58
Art b <94374097+artb433@users.noreply.github.com>
2022-01-20 19:57:06 +0000

    my

--- health_checker/lib/calculator_brain.dart
+++ health_checker/lib/calculator_brain.dart
@@ -22,4 +22,14 @@ class CalculatorBrain {
       return 'Underweight';
     }
   }
+
+  String hint() {
+    if (_bmi >= 25) {
+      return 'You have a higher than normal body, Try to exercise more';
+    } else if (_bmi > 18.5) {
+      return 'You have a normal body weight. Good job';
+    } else {
+      return 'You have a lower than normal body weight. You can eat a bit more';
+    }
+  }
 }
