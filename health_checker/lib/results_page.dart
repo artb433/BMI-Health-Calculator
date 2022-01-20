d577c0e247047ac4d510be81e2117777fef8d0d2
Art b <94374097+artb433@users.noreply.github.com>
2022-01-20 12:39:01 +0000

    my

---
+++ health_checker/lib/results_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  const ResultsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello');
+  }
+}
