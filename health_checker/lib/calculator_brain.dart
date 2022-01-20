f20c58d675cb2aed6e1b2cc3bc1dd9d85f3c9a9b
Art b <94374097+artb433@users.noreply.github.com>
2022-01-20 23:56:01 +0000

    my

--- health_checker/lib/calculator_brain.dart
+++ health_checker/lib/calculator_brain.dart
@@ -6,7 +6,7 @@ class CalculatorBrain {
   final int? height;
   final int? weight;
 
-  final double _bmi = 0;
+  double? _bmi;
 
   String calculateBMI() {
     double _bmi = weight! / pow(height! / 100, 2);
@@ -14,34 +14,21 @@ class CalculatorBrain {
   }
 
   String resultComment() {
-    // if (_bmi >= 25) {
-    //   print('overweight');
-    //   return 'Overweight';
-    // } else if (_bmi > 18.5) {
-    //   print('normal');
-    //   return 'Normal';
-    // } else {
-    //   print('under');
-    //   return 'Underweight';
-    // }
-
-    if (_bmi < 18) {
-      return 'underweight';
-    } else if (_bmi == 18) {
-      return 'overweight';
-    } else if (_bmi > 18.5) {
-      return 'overweight';
+    if (_bmi! >= 25) {
+      return 'Overweight';
+    } else if (_bmi! > 18.5) {
+      return 'Normal';
     } else {
-      return 'unknown';
+      return 'Underweight';
     }
   }
 
   String hint() {
-    if (_bmi >= 25) {
+    if (_bmi! >= 25) {
       return 'You have a higher than normal body, Try to exercise more.';
-    } else if (_bmi > 18.5) {
+    } else if (_bmi! > 18.5) {
       return 'You have a normal body weight. Good job. ';
-    } else if (_bmi < 18.5) {
+    } else if (_bmi! < 18.5) {
       return 'You have a lower than normal body weight. You can eat a bit more.';
     } else {
       return '';
