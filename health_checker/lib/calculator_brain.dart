85ed5116b87e93411433cadd4ab557a318e459ce
Art b <94374097+artb433@users.noreply.github.com>
2022-01-20 19:42:58 +0000

    my

--- health_checker/lib/calculator_brain.dart
+++ health_checker/lib/calculator_brain.dart
@@ -1,6 +1,25 @@
-class CalculatorBrain {
-  final int height;
-  int weight;
+import 'dart:math';
 
-  CalculatorBrain({this.height, this.weight});
+class CalculatorBrain {
+  CalculatorBrain({required this.height, required this.weight});
+
+  final int height;
+  final int weight;
+
+  double _bmi;
+
+  String calculateBMI() {
+    double _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String resultComment() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
 }
