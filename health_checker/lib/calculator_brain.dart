c50e15f005c2077eb39b4c8efcbeef956dc72c49
Art b <94374097+artb433@users.noreply.github.com>
2022-01-25 23:10:55 +0000

    my

--- health_checker/lib/calculator_brain.dart
+++ health_checker/lib/calculator_brain.dart
@@ -3,10 +3,10 @@ import 'dart:math';
 class CalculatorBrain {
   CalculatorBrain({this.height, this.weight});
 
-  final int? height;
-  final int? weight;
+  int height;
+  int weight;
 
-  final double _bmi = 0;
+  double _bmi = 0;
 
   String calculateBMI() {
     double _bmi = weight! / pow(height! / 100, 2);
